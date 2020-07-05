56d5283fa2c002287232c137415777428f61f146
Florent Bariod <fbariod@ripple.com>
2020-07-05 13:11:14 -0400

    added sequence recorder

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,6 +1,6 @@
 #include "ofApp.h"
 
-
+bool recording = false;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
@@ -15,6 +15,20 @@ void ofApp::setup(){
     gui.add(colorBrightnessSlider.setup("brightness", 255, 0, 255));
     gui.add(colorAlphaSlider.setup("alpha", 200, 0, 255));
     gui.add(colorRandomnessSlider.setup("randomness", 50, 0, 255));
+    
+    using namespace std::chrono;
+    milliseconds ms = duration_cast< seconds >(system_clock::now().time_since_epoch());
+    recorder.setPrefix(ofToDataPath("recording/" + ofToString(ms.count()) + "/render/frame_"));
+    recorder.setFormat("png");
+    recorder.setNumberWidth(8);
+    recorder.startThread();
+    
+    ofSetFrameRate(60);
+}
+
+void ofApp::exit() {
+    recorder.waitForThread();
+    recorder.stopThread();
 }
 
 //--------------------------------------------------------------
@@ -29,27 +43,40 @@ void ofApp::draw(){
     for (auto it = walkers.begin(); it != walkers.end(); it++){
         it->draw();
     }
+    
+    if (recording){
+        screenCapture.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+        recorder.addFrame(screenCapture);
+    }
+    
     gui.draw();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    switch (key) {
+        case 'q':
+            recording = !recording;
+            break;
+        default:
+            break;
+    }
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
@@ -62,30 +89,30 @@ void ofApp::mousePressed(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,14 +2,15 @@
 
 #include "ofMain.h"
 #include "ofxGui.h"
+#include "ofxImageSequenceRecorder.h"
 
 class Walker {
 public:
     Walker(float boundaryX, float boundaryY, float startX, float startY, ofColor color, float speedFactor){
         this->boundaryX = boundaryX;
         this->boundaryY = boundaryY;
-        this->velocityX = ofRandom(-2, 2) * speedFactor;
-        this->velocityY = ofRandom(-2, 2) *  speedFactor;
+        this->velocityX = ofRandom(-2, 2);
+        this->velocityY = ofRandom(-2, 2);
         this->line.addVertex(startX, startY);
         this->lastX = startX;
         this->lastY = startY;
@@ -22,10 +23,13 @@ public:
             return;
         }
         
-        velocityX += (ofNoise(lastX * 0.005, lastY * 0.005) - 0.5) * 2.0 * speedFactor;
-        velocityY += abs((ofNoise(lastY * 0.005, lastX * 0.005) - 0.5) * 2.0) * speedFactor;
-        lastX += velocityX;
-        lastY += velocityY;
+        if (ofGetFrameNum() % ((int) (1 / speedFactor)) == 0){
+            velocityX += (ofNoise(lastX * 0.005, lastY * 0.005) - 0.5) * 2.0 ;
+            velocityY += abs((ofNoise(lastY * 0.005, lastX * 0.005) - 0.5) * 2.0) ;
+        }
+        
+        lastX += velocityX * speedFactor;
+        lastY += velocityY * speedFactor;
         line.addVertex(lastX, lastY);
     }
     
@@ -37,7 +41,7 @@ public:
         ofSetColor(color);
         line.draw();
     }
-
+    
     
 private:
     int boundaryX, boundaryY;
@@ -49,34 +53,39 @@ private:
 };
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
     
-        vector<Walker> walkers;
+public:
+    void setup();
+    void exit();
+    void update();
+    void draw();
     
-        ofxFloatSlider speedFactorSlider;
-        ofxIntSlider walkersCountSlider;
-        ofxIntSlider colorHueSlider;
-        ofxIntSlider colorSatSlider;
-        ofxIntSlider colorBrightnessSlider;
-        ofxIntSlider colorAlphaSlider;
-        ofxIntSlider colorRandomnessSlider;
-        ofxPanel gui;
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    vector<Walker> walkers;
+    
+    ofxFloatSlider speedFactorSlider;
+    ofxIntSlider walkersCountSlider;
+    ofxIntSlider colorHueSlider;
+    ofxIntSlider colorSatSlider;
+    ofxIntSlider colorBrightnessSlider;
+    ofxIntSlider colorAlphaSlider;
+    ofxIntSlider colorRandomnessSlider;
+    ofxPanel gui;
+    
+    // recording
+    ofxImageSequenceRecorder recorder;
+    ofImage screenCapture;
 };
 
 
